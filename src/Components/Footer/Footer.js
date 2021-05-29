2929866ae023384e80767de5610b7d09647a2b00
Andrius <andrius2500@gmail.com>
2021-05-29 16:10:05 +0300

    Created footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from './images/4guru.svg';
+import logo from './images/4guru-stretched.jpg';
 
 import './styles/styles.css';
 
@@ -12,17 +12,21 @@ function Footer() {
                     <div className='footer__logo'>
                         <img src={logo} alt='logo'/>
                     </div>
-                    <div>
+                    <div className='footer__contact-info'>
                         <p>Vilnius, Lietuva, Taikos g. 150-12 LT-11111</p>
                         <p>111-111-111</p>
                         <p>info@4guru.com</p>
                     </div>
-                    <div>
+                    <div className='footer__newsletter'>
                         <p>Subscribe to our newsletter</p>
+                        <div>
+                            <input type='email' placeholder='Email' className='footer__input'/>
+                            <button className='footer__subscribe-btn'>Subscribe</button>
+                        </div>
                     </div> 
                 </div>
                 <div className='footer__rights'>
-
+                    <p>@2021 | designed by akgrafika.lt</p>
                 </div>
             </div>
         </div>
